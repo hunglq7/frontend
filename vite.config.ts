210250eb273bb0ef28913d7d39cd0d7cfe58c07e
Le Quang Hung <hunglq7@gmail.com>
2026-08-29 21:05:36 +0700

    feat: update

--- vite.config.ts
+++ vite.config.ts
@@ -37,7 +37,7 @@ const DANHMUC_REWRITE_REGEXP = /^\/danhmuc/;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: isDev ? "/" : "/react-antd-admin/",
+	base: isDev ? "/" : "/",
 	plugins: [
 		// vitePluginFakeServer({
 		// 	basename: "/api",
